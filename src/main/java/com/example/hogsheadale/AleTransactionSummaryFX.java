3d8edb33be418a5b317b5949cc34a55e29e13904
Aaron Pelto <ajpelto@gmail.com>
2023-09-20 04:10:42 -0400

    Update the main ale

--- src/main/java/com/example/hogsheadale/AleTransactionSummaryFX.java
+++ src/main/java/com/example/hogsheadale/AleTransactionSummaryFX.java
@@ -23,35 +23,39 @@ public class AleTransactionSummaryFX extends Application {
         Label titleLabel = new Label("Ale Transaction Summary");
         titleLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
 
+        // Create input fields and labels
         Label hogsheadsLabel = new Label("Number of Hogsheads:");
         TextField hogsheadsTextField = new TextField();
-        hogsheadsTextField.setPromptText("Enter an integer");
+        hogsheadsTextField.setPromptText("Enter a positive integer");
 
         Label amountPerHogsheadLabel = new Label("Dollar Amount per Hogshead:");
         TextField amountPerHogsheadTextField = new TextField();
-        amountPerHogsheadTextField.setPromptText("Enter a decimal");
+        amountPerHogsheadTextField.setPromptText("Enter a non-negative decimal");
 
         Label distanceLabel = new Label("Distance from Hogsmeade (miles):");
         TextField distanceTextField = new TextField();
-        distanceTextField.setPromptText("Enter a decimal");
+        distanceTextField.setPromptText("Enter a positive decimal");
 
-        Button calculateButton = new Button("Calculate");
+        // Create a result area
         TextArea resultTextArea = new TextArea();
         resultTextArea.setEditable(false);
 
+        // Create a calculate button
+        Button calculateButton = new Button("Calculate");
+
         // Instructions label
-        Label instructionsLabel = new Label("Instructions:\n"
-                + "- Enter the number of hogsheads as an integer.\n"
-                + "- Enter the dollar amount per hogshead as a decimal.\n"
-                + "- Enter the distance from Hogsmeade in miles as a decimal.\n"
-                + "- Click the 'Calculate' button to get the summary.");
+        Label instructionsLabel = new Label("Instructions:\n" +
+                "- Enter the number of hogsheads as a positive integer.\n" +
+                "- Enter the dollar amount per hogshead as a non-negative decimal.\n" +
+                "- Enter the distance from Hogsmeade in miles as a positive decimal.\n" +
+                "- Click the 'Calculate' button to get the summary.");
 
         // Calculate button event handler
         calculateButton.setOnAction(event -> {
             try {
-                int numHogsheads = Integer.parseInt(hogsheadsTextField.getText());
-                double dollarAmountPerHogshead = Double.parseDouble(amountPerHogsheadTextField.getText());
-                double distanceMiles = Double.parseDouble(distanceTextField.getText());
+                int numHogsheads = validatePositiveInteger(hogsheadsTextField.getText());
+                double dollarAmountPerHogshead = validateNonNegativeDecimal(amountPerHogsheadTextField.getText());
+                double distanceMiles = validatePositiveDecimal(distanceTextField.getText());
 
                 // Constants for conversion rates and delivery speed
                 final double GALLONS_PER_HOGSHEAD = 54.0;
@@ -72,22 +76,21 @@ public class AleTransactionSummaryFX extends Application {
                 int totalKnuts = (int) ((totalGalleons * GALLEON_TO_SICKLE - totalSickles) * SICKLE_TO_KNUT);
 
                 // Format currency values
-                NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
-                String formattedUSD = currencyFormat.format(totalUSD);
-                String formattedEuros = currencyFormat.format(totalEuros);
-                String formattedPounds = currencyFormat.format(totalPounds);
+                String formattedUSD = formatCurrency(totalUSD);
+                String formattedEuros = formatCurrency(totalEuros);
+                String formattedPounds = formatCurrency(totalPounds);
 
                 String summary = "Amount: " + numHogsheads + " hogsheads, " + String.format("%.1f", totalGallons) + " gallons\n"
                         + "Delivery Time: " + String.format("%.1f", deliveryTimeHours) + " hours\n"
                         + "Cost:\n"
                         + "  U.S. Dollars:       " + formattedUSD + "\n"
-                        + "  Euros:        " + formattedEuros + "\n"
+                        + "  Euros:               " + formattedEuros + "\n"
                         + "  British Pounds: " + formattedPounds + "\n"
                         + "  Galleons: " + (int) totalGalleons + ", Sickles: " + totalSickles + ", Knuts: " + totalKnuts;
 
                 resultTextArea.setText(summary);
-            } catch (NumberFormatException e) {
-                resultTextArea.setText("Invalid input. Please enter valid numbers.");
+            } catch (IllegalArgumentException e) {
+                resultTextArea.setText("Invalid input. " + e.getMessage());
             }
         });
 
@@ -108,5 +111,53 @@ public class AleTransactionSummaryFX extends Application {
         Scene scene = new Scene(vbox, 1280, 720);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private int validatePositiveInteger(String str) {
+        int value;
+        try {
+            value = Integer.parseInt(str);
+            if (value <= 0) {
+                throw new IllegalArgumentException("Number of hogsheads must be a positive integer.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Number of hogsheads must be a positive integer.");
+        }
+        return value;
+    }
+
+    private double validateNonNegativeDecimal(String str) {
+        double value;
+        try {
+            value = Double.parseDouble(str);
+            if (value < 0) {
+                throw new IllegalArgumentException("Dollar amount must be a non-negative decimal.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Dollar amount must be a non-negative decimal.");
+        }
+        return value;
+    }
+
+    private double validatePositiveDecimal(String str) {
+        double value;
+        try {
+            value = Double.parseDouble(str);
+            if (value <= 0) {
+                throw new IllegalArgumentException("Distance must be a positive decimal.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Distance must be a positive decimal.");
+        }
+        return value;
+    }
+
+    private String formatCurrency(double value) {
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        return currencyFormat.format(value);
+    }
+
+    public void stop() {
+        // Clean up any resources if needed
     }
 }
